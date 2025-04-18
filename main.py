c6c4df108f0e86a693f79d7d52854b7e25b05381
Thomas <gamer.my2@gmail.com>
2025-04-18 15:44:21 +0200

    Bookbot Ch2-4

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 file_path = "books/frankenstein.txt"
 
 def main():
-    get_book_text(file_path)
+    text = get_book_text(file_path)
+    num_words = words_count(text)
+    print (f"{num_words} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         book = f.read()
-    print(book)
-    
+    return book
+
+def words_count(book):
+    words_array = book.split()
+    count = len(words_array)
+    return count
     
 main()
