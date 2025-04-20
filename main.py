6693e0807d9e2615bcc1cd56d35b0d14190c433c
Thomas <gamer.my2@gmail.com>
2025-04-20 09:40:07 +0200

    Bookbot Ch2-6

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from stats import get_num_words
+from stats import char_count
 
 file_path = "books/frankenstein.txt"
 
 def main():
+    #print word count
     text = get_book_text(file_path)
     num_words = get_num_words(text)
-    print (f"{num_words} words found in the document")
+    print(f"{num_words} words found in the document")
+    
+    #print letter count
+    lt_count = char_count(text)
+    print(lt_count)
+    
 
 def get_book_text(path):
     with open(path) as f:
