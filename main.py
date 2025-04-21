cc112ee54f2a3cbf654ee1d55c5c0e51ca821699
Thomas <gamer.my2@gmail.com>
2025-04-21 06:02:56 +0200

    Bookbot Ch3-2

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from stats import get_num_words
-from stats import char_count
-from stats import dic_sort
+import sys
+from stats import get_num_words, char_count, dic_sort
 
-file_path = "books/frankenstein.txt"
+#file_path = "books/frankenstein.txt"
 
 def main():
+    #Wrong input exit
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+    
     #print top line
     print("============ BOOKBOT ============")
     #print book file
@@ -15,7 +21,7 @@ def main():
     #print word count
     text = get_book_text(file_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
     
     #print Character  count line
     print("--------- Character Count -------")
@@ -26,7 +32,10 @@ def main():
     for key, value in sort_lt_count.items():
         print(f"{key}: {value}")
         
-        
+    #print end line
+    print("============= END ===============")
+
+     
 def get_book_text(path):
     with open(path) as f:
         book = f.read()
